8fb6b812cb6ff9a843dbba9c50351cbede9f87d3
hauntsmane <32107564+hauntsmane@users.noreply.github.com>
2017-10-02 15:37:50 -0400

    Add files via upload

---
+++ StackCalculator.java
@@ -0,0 +1,18 @@
+package edu.wit.dcsn.comp2000.stackadt;
+
+public class StackCalculator {
+	
+	private final boolean isDigit(char c){
+		return ((c >= 48) && c <= 57); //change to 0 and 9 if this doesnt work
+   }
+	public static void getChar(string){
+	
+		while(string.hasNext){
+			if (string.isDigit()){
+			string.toArray();
+			}
+			else {
+			string.toArray2();	
+			}
+		}
+}
